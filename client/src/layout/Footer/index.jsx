7cbaebbb3a1ef67da093d4262d2cc011b3a5a3ca
Iszdn <tu7k21xbe@code.edu.az>
2023-11-29 14:55:29 +0400

    footer some updates

--- client/src/layout/Footer/index.jsx
+++ client/src/layout/Footer/index.jsx
@@ -1,10 +1,16 @@
 import React from "react";
 import { FaLongArrowAltUp } from "react-icons/fa";
 import { FaPaperPlane } from "react-icons/fa";
+import { FaTwitter } from "react-icons/fa";
+import { FaFacebookF } from "react-icons/fa";
+import { FaYoutube } from "react-icons/fa";
+import { FaGooglePlusG } from "react-icons/fa";
+import { FaInstagram } from "react-icons/fa";
 import "./index.scss";
 const Footer = () => {
   return (
     <footer id="footer">
+    <div className="container">
       <div className="top-footer">
         <div className="container">
           <div className="row">
@@ -50,10 +56,20 @@ const Footer = () => {
                 <img src="https://xpressrow.com/html/cafena/cafena/assets/images/logo/logo.png" alt="" />
                 <p>Donec et nibh maximus, congue est eu, mattis nunc. Praesent ut quam quis quam venenatis fringilla. Morbi vestibulum</p>
               </div>
-            <div className="col-lg-5 align-self-end"></div>
+           
+            </div>
+             <div className="col-lg-5 align-self-end">
+            <div className="social-links d-flex align-items-center justify-content-lg-end">
+            <a><FaTwitter /></a>
+            <a><FaFacebookF /></a>
+            <a><FaYoutube /></a>
+            <a><FaGooglePlusG /></a>
+            <a><FaInstagram /></a>
+            </div>
             </div>
           </div>
         </div>
+      </div>
       </div>
     </footer>
   );
