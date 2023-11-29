133b617aedb0954952082449084d160e4b951bf4
ehmedosmanov <ehmedosmanov@gmail.com>
2023-11-29 12:18:25 +0400

    Created Folder Structure

---
+++ client/src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
+import Main from "./layout/Main";
+import Home from "./pages/Home";
+import "../src/assets/scss/main.scss";
+
+function App() {
+  return (
+    <Routes>
+      <Route path="/" element={<Main />}>
+        <Route index element={<Home/>}/>
+        {/* ABOUT */}
+        {/* MENU */}
+        {/* RESERVATION */}
+        {/* FAQ */}
+        {/* SHOP */}
+        {/* CONTACT */}
+      </Route>
+    </Routes>
+  );
+}
+
+export default App;
