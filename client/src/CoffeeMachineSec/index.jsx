cee2a80968a7e168d039eee4041dddebc1216a04
Iszdn <tu7k21xbe@code.edu.az>
2023-11-30 21:42:56 +0400

    some section was added

---
+++ client/src/CoffeeMachineSec/index.jsx
@@ -0,0 +1,48 @@
+import React from 'react'
+import { LuPlus } from "react-icons/lu";
+import "./index.scss"
+
+const CoffeMachineSection = () => {
+  return (
+    <>
+       {/* coffee-machine section start  */}
+       <section id='coffee-machine'>
+<div className="container">
+  <div className="row">
+    <div className="machine-content col-lg-6 col-md-12 col-12 order-lg-1 order-md-2 order-2">
+      <div className="about">
+        <div className="heading">
+          <h2 className='title'>COFFEE MACHINE,BUY FOR HOME</h2>
+          <p>Donec et nibh maximus, congue est eu, mattis nunc. Praesent ut quam quis quam venenatis fringilla. Morbi vestibulum id tellus mmodo mattis. Aliquam erat volutpat. <br />                                mattis nunc. Praesent ut quam quis quam venenatis fringilla. Morbi vestibulum id tellus mmodo mattis.</p>
+        </div>
+        <div className="btn-m d-flex align-items-center"><a href="">Discover now</a></div>
+      </div>
+    </div>
+    <div className='machine-img col-lg-6 col-md-12 col-12 order-lg-2 order-md-1 '>
+      <div className='big-product-wrapper position-relative'>
+        <div className="img2"> <img src="https://xpressrow.com/html/cafena/cafena/assets/images/products/product-big.png" alt="" />
+         <div className='ph-wrapper'>
+          <div className="product-highlite1">
+            <span className='product-highlight__trigger'>
+            <LuPlus />
+            </span>
+          </div>
+          <div className="product-highlite2">
+            <span className='product-highlight__trigger'>
+            <LuPlus />
+            </span>
+          </div>
+        </div></div>
+       
+       
+      </div>
+    </div>
+  </div>
+</div>
+</section>
+{/* coffee-machine section end  */}
+    </>
+  )
+}
+
+export default CoffeMachineSection
--- client/src/pages/About/index.jsx
+++ client/src/pages/About/index.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
-import Professional from '../../Professional'
  
 import "./index.scss"
+import ProfessionalSection from '../../professionalSection'
+import AboutCafena from '../../AboutCafena'
+
 const About = () => {
   return (
     <>
-<section id='professional'>
-<div className="container">
-    <div className="row">
-        <div className='col-lg-12 text-center'>
-            <div className='section-heading'>
-                <span className='sub-title'>Our Chef</span>
-                <h2 className='title'>Meet our Professional</h2>
-            </div>
-        </div>
-    </div>
-    <Professional></Professional>
-</div>
-</section>
+    
+<AboutCafena></AboutCafena>
+
+
+
+    <ProfessionalSection></ProfessionalSection>
+
+
+
     </>
   )
 }
