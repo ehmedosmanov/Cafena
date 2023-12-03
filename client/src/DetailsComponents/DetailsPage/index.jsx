0714c00341efdd47344d36315d280f569893c752
Iszdn <tu7k21xbe@code.edu.az>
2023-12-03 15:34:59 +0400

    modified

--- client/src/DetailsComponents/DetailsPage/index.jsx
+++ client/src/DetailsComponents/DetailsPage/index.jsx
@@ -1,10 +1,12 @@
 import React from 'react'
 import Products from '../Products'
+import TabDetails from '../TabDetails'
 
 const DeatailsPage = () => {
   return (
     <>
     <Products></Products>
+    <TabDetails></TabDetails>
     </>
   )
 }
