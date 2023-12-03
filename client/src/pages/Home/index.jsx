fdc41dbc2393c2da304dfdd95927091a210a6ce7
ferid aqa <fer2004id@gmail.com>
2023-12-03 11:47:27 -0800

    add manu

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import Feature__items from '../../components/Feature__items'
 import { Our_history } from '../../components/Our_history'
+import Manu from '../../components/CafenaPopularMenu'
 const Home = () => {
   return (
     <>
@@ -8,6 +9,7 @@ const Home = () => {
     <Feature__items></Feature__items>
     
     <Our_history></Our_history>
+    <Manu></Manu>
     </>
   )
 }
